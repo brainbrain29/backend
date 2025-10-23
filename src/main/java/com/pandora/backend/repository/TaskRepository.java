23cd59dbd0bee010cde75e88aac4ba5af98877c9
Grammar0201 <2670765754@qq.com>
2025-10-23 15:10:07 +0800

    任务创建、删除、编辑、查询后端接口
    在20号的版本基础上增加了任务创建、删除、编辑、查询的后端接口；没有写接口文档捏~

--- src/main/java/com/pandora/backend/repository/TaskRepository.java
+++ src/main/java/com/pandora/backend/repository/TaskRepository.java
@@ -1,5 +1,21 @@
 package com.pandora.backend.repository;
 
-public class TaskRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.pandora.backend.entity.Task;
 
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Integer> {
+    // 根据发送者ID查询任务
+    List<Task> findBySenderEmployeeId(Integer senderId);
+    
+    // 根据执行者ID查询任务
+    List<Task> findByAssigneeEmployeeId(Integer assigneeId);
+    
+    // 根据里程碑ID查询任务
+    List<Task> findByMilestoneMilestoneId(Integer milestoneId);
+    
+    // 根据任务状态查询
+    List<Task> findByTaskStatus(Byte taskStatus);
 }
